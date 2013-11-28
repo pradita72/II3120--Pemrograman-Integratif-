cdb79538934257f68a6cd6168fdeade6a8ce9a54
Aditya Pradita S. <pradita_s@yahoo.co.id>
2013-11-28 21:09:28 +0700

    versi PHP dari tugas 2, juga merupakan perbaikan dari tugas 2 kami

---
+++ Tugas2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+  <title>Weaboo Idol</title>
+
+
+<body>
+  <div id="main">
+  <h1>Selamat datang di kontes Weaboo Idol! </h1>
+  <h2>Silakan klik tombol di bawah untuk memilih informasi yang ingin anda dapatkan</h2>
+
+
+  <p>
+  <form action="ShowIdol.php" method="get">
+    <input type="submit" name="show" value="Info Idol" id="submit" />
+  </form>
+  </p>
+  
+  <p>
+  <form action="ShowLagu.php" method="get">
+    <input type="submit" name="show" value="Info Lagu" id="submit" />
+  </form>
+  </p>
+  
+
+   <form action="ShowJuri.php" method="get">
+    <input type="submit" name="show" value="Info Juri" id="submit" />
+  </form>
+  </p>
+  
+  <h1>Created by: </h1>
+  <h2>Aditya Pradita S. (18211001)</h2>
+  <h2>Rakaputra (18211059)</h2>
+
+</body>
+</html> 
